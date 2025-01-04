46d64cbfc8ac26a63aef27b9d45b37d4482256e4
Kokaminho <vouaeu@gmail.com>
2025-01-04 07:47:56 -0300

    button

--- Scripts/CharacterSheet.gd
+++ Scripts/CharacterSheet.gd
@@ -1,2 +1,17 @@
 extends Control
 
+func _ready():
+	$"Inventário".hide()
+
+
+func _on_Button_pressed():
+	$Button/PopupMenu.show()
+	
+
+func _on_PopupMenu_index_pressed(index):
+	if index == 0:
+		$"Inventário".show()
+		
+
+func _on_Fechar_pressed():
+	$"Inventário".hide()
